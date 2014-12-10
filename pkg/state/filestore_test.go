be1752043c4e619eef00381adc1f07e6f76f6c0b
JP <jp@mongohq.com>
2014-12-10 16:13:34 -0600

    save everything before testing values, added update test

--- pkg/state/filestore_test.go
+++ pkg/state/filestore_test.go
@@ -23,6 +23,12 @@ func TestFilestore(t *testing.T) {
 			"123",
 			time.Now().Unix(),
 		},
+		{
+			"anoterlongkey",
+			"somepath/morepath",
+			"1234",
+			time.Now().Unix(),
+		},
 	}
 
 	for _, d := range data {
@@ -31,7 +37,9 @@ func TestFilestore(t *testing.T) {
 			t.Errorf("got error: %s\n", err)
 			t.FailNow()
 		}
+	}
 
+	for _, d := range data {
 		id, ts, err := fs.Select(d.key, d.path)
 		if err != nil {
 			t.Errorf("got error: %s\n", err)
@@ -43,6 +51,52 @@ func TestFilestore(t *testing.T) {
 		if !reflect.DeepEqual(ts, d.ts) {
 			t.Errorf("wanted: %s, got: %s", d.ts, ts)
 		}
+	}
+
+}
+
+func TestFilestoreUpdates(t *testing.T) {
+	fs := NewFilestore("/tmp/transporter.db", 10000*time.Millisecond)
+
+	data := []struct {
+		key  string
+		path string
+		id   interface{}
+		ts   int64
+	}{
+		{
+			"somelongkey",
+			"somepath",
+			"123",
+			time.Now().Unix(),
+		},
+		{
+			"somelongkey",
+			"somepath",
+			"1234",
+			time.Now().Add(10 * time.Second).Unix(),
+		},
+	}
+
+	for _, d := range data {
+		err := fs.Save(d.key, d.path, &message.Msg{Id: d.id, Timestamp: d.ts})
+		if err != nil {
+			t.Errorf("got error: %s\n", err)
+			t.FailNow()
+		}
+	}
+
+	d := data[len(data)-1]
+	id, ts, err := fs.Select(d.key, d.path)
+	if err != nil {
+		t.Errorf("got error: %s\n", err)
+		t.FailNow()
+	}
+	if !reflect.DeepEqual(id, d.id) {
+		t.Errorf("wanted: %s, got: %s", d.id, id)
+	}
+	if !reflect.DeepEqual(ts, d.ts) {
+		t.Errorf("wanted: %s, got: %s", d.ts, ts)
 	}
 
 }
